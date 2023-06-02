43866a9fa4817337278e4ca3206ec760ba13be4e
eggypriambodo <eggypriambodo86@gmail.com>
2023-06-03 03:56:25 +0700

    database

--- database.js
+++ database.js
@@ -1,21 +1,28 @@
 const mysql = require("mysql");
 
+const DB_HOST = "104.198.38.230";
+const DB_NAME = "mymoney";
+const DB_USER = "root";
+const DB_PASS = "projektcc";
+const INSTANCE_CONNECTION_NAME = "tugas-akhir-praktcc:us-central1:money-tracker-db";
+const NODE_ENV = "value: production";
+
 var config = {
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASS,
+  user: DB_USER,
+  database: DB_NAME,
+  password: DB_PASS,
 };
 
 // Later on when running from Google Cloud, env variables will be passed in container cloud connection config
-if (process.env.NODE_ENV === "production") {
+if (NODE_ENV === "production") {
   console.log("Running from cloud. Connecting to DB through GCP socket.");
-  config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
+  config.socketPath = `/cloudsql/${INSTANCE_CONNECTION_NAME}`;
 }
 
 // When running from localhost, get the config from .env
 else {
   console.log("Running from localhost. Connecting to DB directly.");
-  config.host = process.env.DB_HOST;
+  config.host = DB_HOST;
 }
 
 let connection = mysql.createConnection(config);
