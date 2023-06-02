68a56043de0b788072b5bbb29c8fe8b50db39cd5
eggypriambodo <eggypriambodo86@gmail.com>
2023-06-03 03:34:47 +0700

    database

--- routes/record.js
+++ routes/record.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const router = express.Router();
 const Multer = require("multer");
 const imgUpload = require("../modules/imgUpload");
-const connection = require("../database");
+const connection = require("database.js");
 
 const multer = Multer({
   storage: Multer.MemoryStorage,
